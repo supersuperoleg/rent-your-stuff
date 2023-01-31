3c582ad8c1110a126923ddc954b91d5acb418a8a
Oleg <oleg@MacBook-Pro-Oleg.local>
2023-02-01 00:14:06 +0500

    initital commit

---
+++ src/main/java/com/rakhimov/jmix/rys/screen/user/UserBrowse.java
@@ -0,0 +1,12 @@
+package com.rakhimov.jmix.rys.screen.user;
+
+import com.rakhimov.jmix.rys.entity.User;
+import io.jmix.ui.navigation.Route;
+import io.jmix.ui.screen.*;
+
+@UiController("rys_User.browse")
+@UiDescriptor("user-browse.xml")
+@LookupComponent("usersTable")
+@Route("users")
+public class UserBrowse extends StandardLookup<User> {
+}
---
+++ src/test/java/com/rakhimov/jmix/rys/RentYourStuffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakhimov.jmix.rys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentYourStuffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
